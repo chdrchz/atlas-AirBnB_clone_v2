20f9f370f549e8b45ffa46b4dbeb690b4cd26b6e
chdrchz <saavannaa.rose@gmail.com>
2024-03-08 13:46:11 -0600

    removing unnecesary spaces

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -41,7 +41,7 @@ def number_template(n: int):
 def number_odd_or_even(n: int):
     value = 'even' if n % 2 == 0 else 'odd'
     return render_template('6-number_odd_or_even.html', n=n, value=value)
- 
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
