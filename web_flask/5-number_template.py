1dafb05e2b0aa17e4732c16fd70ce1192f5f15c7
chdrchz <saavannaa.rose@gmail.com>
2024-03-08 12:52:43 -0600

    forgot to add int:n in check_num_template app route

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -32,7 +32,7 @@ def check_num(n):
     return f"{n} is a number"
 
 
-@app.route('/number_template/<n>', strict_slashes=False)
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def check_num_template(n: int):
     return render_template('5-number.html', n=n)
 
