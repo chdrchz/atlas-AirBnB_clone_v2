56aae156c36396aab55ab9326e084f6d6df7eac6
chdrchz <saavannaa.rose@gmail.com>
2024-03-08 13:45:32 -0600

    had ending quote in the wrong place

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -37,7 +37,7 @@ def number_template(n: int):
     return render_template('5-number.html', n=n)
 
 
-@app.route('/number_odd_or_even/<int:n>, strict_slashes=False')
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n: int):
     value = 'even' if n % 2 == 0 else 'odd'
     return render_template('6-number_odd_or_even.html', n=n, value=value)
