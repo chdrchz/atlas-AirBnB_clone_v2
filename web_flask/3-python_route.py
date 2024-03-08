c82815dc549f7539f29d0fb234259425751125f6
chdrchz <saavannaa.rose@gmail.com>
2024-03-08 11:34:52 -0600

    adding a second app.route for python default text value

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -21,10 +21,11 @@ def c(text):
     return "C " + text.replace('_', ' ')
 
 
+@app.route('/python/', defaults={'text': 'is_cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python(text="is_cool"):
+def python(text):
     return "Python " + text.replace('_', ' ')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
